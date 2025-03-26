22fd6bae8dcc2895b7d001ea1c50ae90d42e3c87
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-03-26 12:07:33 +0700

    update lagi

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const admin = require('firebase-admin');
 const serviceAccount = require(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
   databaseURL: process.env.FIREBASE_DB_URL
 });
 
