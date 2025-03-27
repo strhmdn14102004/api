dffc0485e12db63f340c9e5303825b65308d09f1
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-03-27 11:19:01 +0700

    back to awal

--- models/Transaction.js
+++ models/Transaction.js
@@ -8,7 +8,7 @@ const transactionSchema = new mongoose.Schema({
   },
   itemType: { 
     type: String, 
-    enum: ['imei', 'bypass', 'fmi-off'],
+    enum: ['imei', 'bypass'], 
     required: true 
   },
   itemId: { 
