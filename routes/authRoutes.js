801b5f49722b2f00d860f58bdec876abb9ecd540
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-03-27 13:53:38 +0700

    update reset password

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authenticate = require('../middlewares/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/request-reset-password', authController.requestResetPassword);
+router.post('/reset-password', authController.resetPassword);
+router.post('/update-password', authenticate, authController.updatePassword);
 
 module.exports = router;
