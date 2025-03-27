b6aab67f09fd281fe0ffb809fdabde57329c5d79
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-03-27 14:37:50 +0700

    update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,10 +3,19 @@ const secretKey = process.env.JWT_SECRET;
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Akses ditolak, token tidak tersedia' });
+  if (!token) {
+    return res.status(401).json({ 
+      message: 'Akses ditolak, token tidak tersedia' 
+    });
+  }
   
   jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) return res.status(403).json({ message: 'Token tidak valid' });
+    if (err) {
+      return res.status(403).json({ 
+        message: 'Token tidak valid',
+        error: err.message 
+      });
+    }
     req.user = decoded;
     next();
   });
