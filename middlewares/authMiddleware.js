dc4e5eff9f29f7d4890f16a1eb16be4fb4421d22
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-03-27 14:54:53 +0700

    balikin reset password

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,19 +3,10 @@ const secretKey = process.env.JWT_SECRET;
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  if (!token) {
-    return res.status(401).json({ 
-      message: 'Akses ditolak, token tidak tersedia' 
-    });
-  }
+  if (!token) return res.status(401).json({ message: 'Akses ditolak, token tidak tersedia' });
   
   jwt.verify(token, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ 
-        message: 'Token tidak valid',
-        error: err.message 
-      });
-    }
+    if (err) return res.status(403).json({ message: 'Token tidak valid' });
     req.user = decoded;
     next();
   });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,8 +4,5 @@ const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
-router.post('/request-reset-password', authController.requestResetPassword);
-router.get('/verify-reset-token/:token', authController.verifyResetToken);
-router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
