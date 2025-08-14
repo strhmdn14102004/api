3765ce72f75ef7e730032e94ae0e1838408ec5da
Satria Ramadan <158995888+strhmdn14102004@users.noreply.github.com>
2025-08-14 17:15:43 +0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,7 @@ const authController = require('../controllers/authController');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/verify-otp', authController.verifyOtp);
+router.post('/resend-otp', authController.resendOtp);
+router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
